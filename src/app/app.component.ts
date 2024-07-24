be85b69be7e14d9e26aff8071758bd0432ab2a53
Usman <usman242@gmail.com>
2024-07-24 07:43:36 +0400

    adding ngFor with index and track changes add index check for even odd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,4 +31,32 @@ export class AppComponent {
 
   /*--ngSwitch--*/
   viewMode = 'map';
+
+  /*--ngFor-- */
+  
+  courseList:{id:number,name:string}[] = [
+    {id:1, name:"course 1"},
+    {id:2, name:"course 2"},
+    {id:3, name:"course 3"},
+    {id:4, name:"course 4"},
+  ];
+  onAdd(){
+    this.courseList.push({id:5, name:"course 6"});
+  }
+  onRemove(course:{id:number,name:string}){
+    let index = course.id;
+
+    this.courseList.splice(index,1)
+  }
+  loadCourses(){
+    this.courseList =  [
+      {id:6, name:"course 6"},
+      {id:7, name:"course 7"},
+      {id:8, name:"course 8"},
+      {id:9, name:"course 9"},
+    ];
+  }
+  trackCourse(index:number,course:{id:number,name:string}){
+    return course ? course.id : undefined
+  }
 }
