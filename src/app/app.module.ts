ed3cc9a502bf6e680090ec0765024078de6dfd50
Usman <usman242@gmail.com>
2024-08-06 07:32:42 +0400

    creat simple form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,    
+    AppComponent,
+    ContactFormComponent,    
   ],
   imports: [
     BrowserModule,
